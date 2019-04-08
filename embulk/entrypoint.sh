a8a63ac1f46fcd15a1c16dd334a7eb7ef7f6f995
Imaoka Hisatoshi <i.toshihisa@gmail.com>
2019-04-08 22:28:16 +0900

    embulk: base jre-slim & entrypoint.sh fix

--- embulk/entrypoint.sh
+++ embulk/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e
 
-# cp -rf /src/conf/common.${EMBULK_ENV-production} -T /src/conf/common
+if [ "$1" = 'embulk' ]; then
+  # cp -rf /src/conf/common.${EMBULK_ENV-production} -T /src/conf/common
+
+  # drop $1
+  shift
+  exec embulk "$@"
+fi
+
 exec "$@"
