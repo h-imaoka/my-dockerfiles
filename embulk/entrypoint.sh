7e20d51cd6a2456ff70ebd19cc5acb53f3580355
Imaoka Hisatoshi <i.toshihisa@gmail.com>
2020-01-19 22:24:31 +0900

    0.9.22 & base jre = buster + corretto8

--- embulk/entrypoint.sh
+++ embulk/entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 set -e
 
-if [ "$1" = 'embulk' ]; then
-  # cp -rf /src/conf/common.${EMBULK_ENV-production} -T /src/conf/common
-
-  # drop $1
-  shift
-  exec embulk "$@"
-fi
-
 exec "$@"
